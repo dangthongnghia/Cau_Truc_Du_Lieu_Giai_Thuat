8c8e1037e8cb47aaa9fb7c7888074bd1dd016049
Dang Thong Nghia <dangnghia2385@gmail.com>
2024-03-17 10:03:52 +0700

    upadate

--- CopilotAI/BTS.py
+++ CopilotAI/BTS.py
@@ -1,4 +1,4 @@
-class Node:
+class Node:                    # Class Node của cây nhị phân
     def __init__(self, key):
         self.left = None
         self.right = None
@@ -6,54 +6,54 @@ class Node:
 
 
 
-class BST:
+class CayNhiPhan:                           # Class cây nhị phân
     def __init__(self):
         self.root = None
 
-    def insert(self, key):
+    def insert(self, key):                      # Hàm chèn giá trị vào cây
         if self.root is None:
             self.root = Node(key)
         else:
             self._insert(self.root, key)
 
-    def _insert(self, node, key):
-        if key < node.val:
-            if node.left is None:
-                node.left = Node(key)
-            else:
-                self._insert(node.left, key)
-        elif key > node.val:
-            if node.right is None:
+    def _insert(self, node, key):                       # Hàm chèn giá trị vào cây
+        if key < node.val:                      # Nếu giá trị cần chèn nhỏ hơn giá trị của node
+            if node.left is None:                    
+                node.left = Node(key)           # Nếu node.left là None thì chèn giá trị vào đây
+            else:                               
+                self._insert(node.left, key)    # Nếu không thì gọi đệ quy với node.left
+        elif key > node.val:                    # Nếu giá trị cần chèn lớn hơn giá trị của node
+            if node.right is None:              # Nếu node.right là None thì chèn giá trị vào đây
                 node.right = Node(key)
             else:
-                self._insert(node.right, key)
+                self._insert(node.right, key)   
 
-    def preorder(self, node):
+    def Duyet_NLR(self, node):                  # Duyệt theo kiểu NLR
         if node:
             print(node.val)
-            self.preorder(node.left)
-            self.preorder(node.right)
+            self.Duyet_NLR(node.left)
+            self.Duyet_NLR(node.right)
 
-    def postorder(self, node):
+    def Duyet_LRN(self, node):                  # Duyệt theo kiểu LRN
         if node:
-            self.postorder(node.left)
-            self.postorder(node.right)
+            self.Duyet_LRN(node.left)
+            self.Duyet_LRN(node.right)
             print(node.val)
 
-    def inorder(self, node):
+    def Duyet_LNR(self, node):                  # Duyệt theo kiểu LNR
         if node:
-            self.inorder(node.left)
+            self.Duyet_LNR(node.left)
             print(node.val)
-            self.inorder(node.right)
+            self.Duyet_LNR(node.right)
 
 
-bst = BST()
+bst = CayNhiPhan()               
 
-def menu():
+def menu():                             # Cái này tạo menu cho zuiii
     while True:
         import os 
         os.system('cls')
-        print("1. Nhập dữ liệu cho cây")
+        print("1. Nhập dữ liệu cho cây")        
         print("2. Duyệt theo kiểu NLR")
         print("3. Duyệt theo kiểu LRN")
         print("4. Duyệt theo kiểu LNR")
@@ -66,20 +66,20 @@ def menu():
             
         elif choice == 2:
             print("Duyệt theo kiểu NLR:")
-            bst.preorder(bst.root)
+            bst.Duyet_NLR(bst.root)
             os.system('pause')
         elif choice == 3:
             print("Duyệt theo kiểu LRN:")
-            bst.postorder(bst.root)
+            bst.Duyet_LRN(bst.root)
             os.system('pause')
         elif choice == 4:
             print("Duyệt theo kiểu LNR:")
-            bst.inorder(bst.root)
+            bst.Duyet_LNR(bst.root)
             os.system('pause')
         elif choice == 5:
             break
         else:
-            print("Invalid choice")
+            print("Lựa chọn không hợp lệ")
 
 
 
