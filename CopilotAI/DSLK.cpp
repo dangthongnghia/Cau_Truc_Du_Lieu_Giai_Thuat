afafe01d3a53cce33c0442a50255fcf79c64742e
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2024-04-01 14:31:04 +0700

    update

--- CopilotAI/DSLK.cpp
+++ CopilotAI/DSLK.cpp
@@ -1,1 +1,61 @@
+#include<stdio.h>
+#include<iostream>
+struct node
+{
+    int data;
+    node *next;
+};
+//typedef struct node node;
+node* makeNode(int x){
+    node *newNode = new node();
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
+}
+void duyet(node *head){
+    while(head != NULL){
+       std:: cout << head->data << " ";
+        head = head->next;      //  head->next dia chi cua node ke tiep
+    }
+}
+// In ra so luong node trong DSLK
+int count(node *head){
+    int count = 0;
+    while(head != NULL){
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+// Them 1 node vao dau  DSLK
+void addHead(node **head, int x){
+    node *newNode = makeNode(x);
+    newNode->next = *head;
+    *head = newNode;
+}
+// Add 1 node vao cuoi DSLK
+void addTail(node **head, int x){
+    node *newNode = makeNode(x);
+    if(*head == NULL){
+        *head = newNode;
+    }
+    else{
+        node *temp = *head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}   
+
+int main(){
+    node *head = NULL;
+    for (int i = 1; i <= 10; i++ )
+{
+   // addTail(&head, i);
+}
+    duyet(head);
+    //std::cout << count(head);   
+    return 0;
+}
 
---
+++ DSLK.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+// Linked list class
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Insert a new node at the beginning of the list
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    // Display the elements of the list
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    // Insert elements into the list
+    list.insert(5);
+    list.insert(10);
+    list.insert(15);
+
+    // Display the list
+    list.display();
+
+    return 0;
+}
